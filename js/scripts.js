bdac62a784a567c27ce3337c4832496e659c4415
Ewa Manek and Seth Kendall <seth@>
2016-08-17 10:25:02 -0700

    Add logic for specs dealing with single word input

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,26 @@
 var toPigLatin = function(words){
   var vowelArray = ["a", "e", "i", "o", "u"];
   words = words.toLowerCase();
+  var wordArray = words.split(" ");
+  console.log(wordArray);
   if(vowelArray.includes(words.charAt(0))){
     words += "ay";
+  } else {
+    var suffix = "";
+    for(var i = 0; i < words.length; i++){
+      if(!vowelArray.includes(words.charAt(i))){
+        suffix += words.charAt(i);
+      } else {
+        if(words.charAt(i) === vowelArray[4] && words.charAt(i-1) === 'q'){
+          suffix += words.charAt(i);
+          words = words.slice(i+1);
+        } else {
+          words = words.slice(i);
+        }
+        break;
+      }
+    }
+    words += (suffix + "ay");
   }
   return words;
 };
